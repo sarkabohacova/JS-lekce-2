4f8c7fea6b7dbf9d0cd42f41b0aa59e5e2797837
sarkabohacova <bohacova.sar@gmail.com>
2023-10-31 21:30:05 +0100

    another simple commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const jmeno = "sarka"
+const fullPrice = 12;
+const visitorsMonthly = 174;
+const showsMonthly = 15;
+const intake = fullPrice * visitorsMonthly * showsMonthly;
+document.body.innerHTML += '<p>Příjem divadla: ' + intake + ' Kč</p>';
 
-jmeno = "sarka"
+const studentPrice = 0.65 * fullPrice;
+const students = 0.4;
+const intakeStudents = (fullPrice * (1 - students) + studentPrice * students) * visitorsMonthly * showsMonthly;
+document.body.innerHTML += '<p>Příjem divadla se slevou pro studenty:' + intakeStudents + ' Kč </p>';
